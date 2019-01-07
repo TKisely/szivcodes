d48adc3997859a552dc610924cb5e9b1a8c703be
Tamas Kisely <tamaskisely@gmail.com>
2019-01-07 11:19:09 +0100

    Create oraimunka_0107.cpp

---
+++ oraimunka_0107.cpp
@@ -0,0 +1,48 @@
+// oraimunka_0107.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+//megfelelo include-ok biztositasa
+//vector hasznalata -> vector-t include-olni kell
+//string hasznalata -> ...
+
+#include <iostream>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+	//vector az adatoknak (meg kell adnom a tipusat)
+	vector<int> adat;
+
+	//vector az elvalasztasoknak (meg kell adnom a tipusat)
+	vector<int> elvalasztasok;
+
+	//beolvasas valtozo
+	int eppenBeolvasott;
+
+	//beolvassuk a szamsort egy ciklussal (while javasolt)
+	//a beolvasasi valtozot hasznaljuk, 
+	//addig megyunk, mig nem 0-t ad be a felhasznalo
+	//ha epp nem nulla, akkor a valtozo erteket betesszuk a megfelelo vectorba
+
+
+
+	//beolvassuk az elvalasztasokat egy ciklussal (while javasolt)
+
+
+
+	//ennel a pontnal a ket vektorunk fel van toltve a megfelelo ertekekkel
+
+
+
+	//kiiras
+	//egy ciklussal kiirjuk a szamokat egymas utan az adat vectorbol
+	//a cikluson belul if elagazassal vizsgaljuk, hogy kell-e szokoz
+	//ha kell, akkor nem az adat vectorbol a kovetkezo szamot olvasom ki
+	//hanem csak egy szokozt irok ki, majd folytatom tovabb az adat vector-t
+
+
+    std::cout << "Hello World!\n"; 
+}
